fd5ef3622aa33e7f211304b0ab2b945b042eab22
mon <sukontaprapun@gmail.com>
2026-08-12 00:14:31 +0700

    add: add func handleBtnClick

--- src/app/versary/memories/components/CardButton.jsx
+++ src/app/versary/memories/components/CardButton.jsx
@@ -6,37 +6,67 @@ import Image from "next/image";
 function CardButton({ label, desc, img }) {
     const bgBoxRef = useRef(null);
     const boxRef = useRef(null);
-    const [isVisible, setIsVisible] = useState(false);
+    const btnRef = useRef(null);
+    const hasAnimatedRef = useRef(false);
 
-    const toggle = () => {
-        if (isVisible) {
-            gsap.to([bgBoxRef.current, boxRef.current], {
-                autoAlpha: 0,
-                duration: 0.3,
-                ease: "power2.out",
-            });
-        } else {
-            gsap.to([bgBoxRef.current, boxRef.current], {
-                autoAlpha: 1,
-                duration: 0.3,
-                ease: "power2.out",
-            });
-        }
+    const handleBtnClick = () => {
+        // ตอนแสดงผล
+        if (hasAnimatedRef.current) return;
+        hasAnimatedRef.current = true;
+        const tl = gsap.timeline();
 
-        setIsVisible((prev) => !prev);
+        tl.to(bgBoxRef.current, {
+            autoAlpha: 1,
+            duration: 0.5,
+            ease: "power2.out",
+            background: "linear-gradient(to bottom, #f9a8d4, #fdf2f8)",
+            boxShadow: "0 4px 12px rgba(0, 0, 0, 0.04)",
+        })
+            .fromTo(
+                boxRef.current,
+                {
+                    autoAlpha: 0,
+                    width: "0%",
+                    scale: 1,
+                    y: 8,
+                    filter: "blur(2px)",
+                },
+                {
+                    autoAlpha: 1,
+                    width: "100%",
+                    scale: 1,
+                    y: 0,
+                    filter: "blur(0px)",
+                    duration: 1,
+                    ease: "power3.inOut",
+                },
+                "-=0.55",
+            )
+            .fromTo(
+                btnRef.current,
+                {
+                    autoAlpha: 0,
+                    y: 10,
+                },
+                {
+                    autoAlpha: 1,
+                    y: 0,
+                    duration: 0.5,
+                    ease: "power2.out",
+                },
+                "-=0.45",
+            );
     };
 
     return (
         <div className="relative">
-            <div
-                ref={bgBoxRef}
-                className="bg-box absolute inset-0 rounded-2xl bg-gradient-to-b from-pink-300 to-pink-50 shadow-sm"
-            />
+            <div ref={bgBoxRef} className="bg-box absolute inset-0 rounded-2xl" />
             <div className="relative flex flex-col items-center gap-2 rounded-2xl border-pink-100 p-2">
                 <button
-                    onClick={toggle}
+                    onClick={handleBtnClick}
+                    ref={btnRef}
                     type="button"
-                    className="relative z-10 rounded-full border border-pink-100 bg-white px-6 py-1.5 text-sm font-semibold text-pink-600 shadow-sm transition-colors hover:bg-pink-500 hover:text-white"
+                    className=" translate-y-30 relative z-10 rounded-full border border-pink-100 bg-white px-6 py-1.5 text-sm font-semibold text-pink-600 shadow-sm transition-colors hover:bg-pink-500 hover:text-white"
                 >
                     {label}
                 </button>
